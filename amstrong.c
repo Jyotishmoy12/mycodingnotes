793f16eddb24e76870d6de9af18e8658d83377a5
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2022-06-05 18:55:29 +0530

    Create amstrong.c

---
+++ amstrong.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main(){
+int n,r,sum=0,temp;
+printf("enter a number:");
+scanf("%d",&n);
+temp=n;
+while(n>0){
+    r=n%10;
+    sum=sum+r*r*r;
+    n=n/10;
+}
+n=temp;
+if(n==sum){
+    printf("amstrong number");
+}
+else{
+    printf("not amstrong number");
+}
+
+
+    return 0;
+}
