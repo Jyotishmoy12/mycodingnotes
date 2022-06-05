b68d2ee1720498373b0bcff8a661331583946c26
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2022-06-05 18:52:29 +0530

    Create fibonaci.c

---
+++ fibonaci.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+
+int N,i, a=-1, b=1, c;
+printf("enter a number:");
+scanf("%d",&N);
+for(i=0;i<N;i++){
+    c=a+b;
+    printf("%d", c);
+    a=b;
+    b=c;
+}
+
+
+    return 0;
+}
