2fd3aa3ebfb1cc2b3515e18aaf59d2f7236aeed8
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2022-06-12 18:02:27 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+ int a,b;
+ cin>>a>>b;
+ char op;
+ cin>>op;
+ switch(op){
+     
+     case '+': cout<<(a+b)<<endl;
+               break;
+
+
+     case '-': cout<<(a-b)<<endl;
+                break;
+
+
+     case '*': cout<<(a*b)<<endl;
+                break;
+
+
+     case '/': cout<<(a/b)<<endl;
+               break;
+
+
+     case '%': cout<<(a%b)<<endl;
+               break;
+
+
+     default: cout<<"enter a valid operation"<<endl;
+   }
+
+ return 0;
+}
