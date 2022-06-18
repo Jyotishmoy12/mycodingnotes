fecbd6ddbe8f5b29abd9d2b69e33868f080ff220
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2022-06-18 12:43:29 +0530

    Create printnamestringbystring.c

---
+++ printnamestringbystring.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void printString(char arr[]);
+int main(){
+char firstName[]="Jyotishmoy";
+char lastName[]="Deka";
+
+printString(firstName);
+printString(lastName);
+return 0;
+}
+void printString(char arr[]){
+for(int i=0; arr[i] !='\0'; i++){
+printf("%c", arr[i]);
+} 
+printf("\n");
+}
