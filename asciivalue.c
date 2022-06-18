2c76e98f8c013ab7efaba65a012d4af5b3a6c83f
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2022-06-18 11:57:00 +0530

    Create asciivalue.c

---
+++ asciivalue.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int i;
+char name[30];
+printf("enter your name\n:");
+scanf("%s",&name);
+for(i=0;i<name[i];i++){
+    printf("ascii value of %c is %d\n", name[i],name[i]);
+}
+return 0;
+}
