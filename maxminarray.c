11e33d08c19741e803406f809a063e14c2cd7103
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2022-06-24 01:27:43 +0530

    Update maxminarray.c

--- maxminarray.c
+++ maxminarray.c
@@ -1,26 +1,24 @@
 
 #include <stdio.h>
-//#include<math.h>
 int largest(int arr[], int n)
 {
-	int i;
-	int max=arr[0];
-	for (i = 1; i < n; i++)
-		if (arr[i] > max)
-			max = arr[i];
-
-	return max;
+int i;
+int max=arr[0];
+for (i = 1; i < n; i++)
+if (arr[i] > max)
+max = arr[i];
+return max;
 }
 
 int main()
 {
-    int arr[10000];
-    int n ;
-    int i;
-    scanf("%d", &n);
-    for ( i = 0; i < n; i++){
-        scanf("%d", &arr[i]);
-    }
-	printf("Largest in given array is %d", largest(arr, n));
-	return 0;
+ int arr[10000];
+ int n ;
+ int i;
+ scanf("%d", &n);
+ for ( i = 0; i < n; i++){
+ scanf("%d", &arr[i]);
+  }
+printf("Largest in given array is %d", largest(arr, n));
+return 0;
 }
