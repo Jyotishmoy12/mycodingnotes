4b3c3143390d94645a62e5d286993da3be430e7a
Jyotishmoy Deka <93263133+Jyotishmoy12@users.noreply.github.com>
2022-07-01 20:38:17 +0530

    Update printallprime.c

--- printallprime.c
+++ printallprime.c
@@ -5,14 +5,11 @@ printf("enter two numbers:");
 scanf("%d",&l);
 scanf("%d",&u);
 for(x=l; x<u; x++){
-    for(i=2; i<x;i++)
-        if(x%i==0)
-        break;
-    if(i==x)
-    printf("%d",x);
+for(i=2; i<x;i++)
+if(x%i==0)
+break;
+if(i==x)
+printf("%d",x);
 }
-
-
-
-    return 0;
+return 0;
 }
